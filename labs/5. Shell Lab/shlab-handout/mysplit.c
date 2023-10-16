a1bf9c9f9cbe6e9e9f2f679cdcde6986b8493f72
lumingfan <2507550027@qq.com>
2023-10-16 19:28:02 +0800

    shell lab: solve rebase conflict

--- labs/5. Shell Lab/shlab-handout/mysplit.c
+++ labs/5. Shell Lab/shlab-handout/mysplit.c
@@ -1,40 +1,4 @@
 /* 
-<<<<<<< HEAD
- * mysplit.c - Another handy routine for testing your tiny shell
- * 
- * usage: mysplit <n>
- * Fork a child that spins for <n> seconds in 1-second chunks.
- */
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-
-int main(int argc, char **argv) 
-{
-    int i, secs;
-
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <n>\n", argv[0]);
-	exit(0);
-    }
-    secs = atoi(argv[1]);
-
-
-    if (fork() == 0) { /* child */
-	for (i=0; i < secs; i++)
-	    sleep(1);
-	exit(0);
-    }
-
-    /* parent waits for child to terminate */
-    wait(NULL);
-
-    exit(0);
-}
-=======
  * mysplit.c - Shell lab test program
  * 
  * Forking version of the myspin programs. Useful for testing
@@ -128,4 +92,3 @@ int main(int argc, char **argv)
     exit(0);
 }
 
->>>>>>> 12e09c8 (shell lab: add shell lab start code and write up)
