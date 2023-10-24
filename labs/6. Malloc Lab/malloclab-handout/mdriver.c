6fd77b19b33241683be44bf1ace72376fb49dff5
lumingfan <2507550027@qq.com>
2023-10-24 22:50:33 +0800

    malloc lab: modify mdriver.c to make it not post result to Autolab

--- labs/6. Malloc Lab/malloclab-handout/mdriver.c
+++ labs/6. Malloc Lab/malloclab-handout/mdriver.c
@@ -486,10 +486,10 @@ int main(int argc, char **argv)
 	}
 
 	/* Post result to Autolab */
-	sprintf(autoresult, "%d:%.0f:%.0f:%.0f",
-			numcorrect, (float)perfindex, 
-			avg_mm_throughput/1000.0, avg_mm_util*100);
-	driver_post(NULL, autoresult, autograder, status_msg);
+	// sprintf(autoresult, "%d:%.0f:%.0f:%.0f",
+	// 		numcorrect, (float)perfindex, 
+	// 		avg_mm_throughput/1000.0, avg_mm_util*100);
+	// driver_post(NULL, autoresult, autograder, status_msg);
 
 	exit(0);
 }
